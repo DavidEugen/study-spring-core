8da5c3e1c3bf7f382862ca3e72486d158ff7f776
DavidEugen <platonicojju@gmail.com>
2022-03-20 19:58:39 +0900

    DOC : DIP 위반 사례 ( NPE 발생 )
    SOLID 원칙의 DIP 위반.
    구체 클래스를 의존 하지 않도록 설정 필요 => 설정 conifg 관리 필요

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,12 +10,24 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy disoucntPolicy = new FixDiscountPolicy();
+
+    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    /**
+     * 문제점 발생,
+     * DiscountPolicy policy 라는 인터페이스에 의존하지만
+     * FixDiscountPolicy라는 구체클래스에도 의존하여
+     * 정책변경으로 클라이언트의 소스가 변경되어야 함
+     * DIP 위반
+     */
+
+    private DiscountPolicy discountPolicy; //final 이 사라진 이유 : final은 값이 할당 되어야 하니깐
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member findMember = memberRepository.findById(memberId);
-        int discountPrice = disoucntPolicy.discount(findMember, itemPrice);
+        int discountPrice = discountPolicy.discount(findMember, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
 
