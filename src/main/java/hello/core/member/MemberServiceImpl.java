b82267c57567c935f9fc823f6b16eece76b670f5
DavidEugen <platonicojju@gmail.com>
2022-03-20 20:27:25 +0900

    Feat : AppConfig 설정 추가 (미완)
    개별 구현체에 대한 선정은 설정을 담당하는 역할을 두어 처리.
    각 객체들은 어떤 구현체가 오던, 각자의 역할만 충실히 수행하도록 함.

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,16 +1,16 @@
 package hello.core.member;
 
-public class MemberServiceImpl implements MemberService{
+public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memebrRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
     public void join(Member member) {
-        memebrRepository.save(member);
+        memberRepository.save(member);
     }
 
     @Override
     public Member findMember(Long memberId) {
-        return memebrRepository.findById(memberId);
+        return memberRepository.findById(memberId);
     }
 }
