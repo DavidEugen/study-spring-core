3e63a64a57e82dae86ac35ec98b856489f913b91
DavidEugen <platonicojju@gmail.com>
2022-03-20 20:43:56 +0900

    Fix: AppConfig 의존 주입 설정 (미완)
    필요한 구현체들을 AppConfig 에서 주입해 주는 역할을 한다. => 관심사의 분리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,10 +9,11 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
 
     /**
      * 문제점 발생,
@@ -21,8 +22,13 @@ public class OrderServiceImpl implements OrderService {
      * 정책변경으로 클라이언트의 소스가 변경되어야 함
      * DIP 위반
      */
-
+    private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy; //final 이 사라진 이유 : final은 값이 할당 되어야 하니깐
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
