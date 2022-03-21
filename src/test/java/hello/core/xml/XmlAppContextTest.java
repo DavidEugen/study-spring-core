6f37d0d40b80ab10ffb61086adcd60ecfe26ab8e
DavidEugen <platonicojju@gmail.com>
2022-03-21 23:34:48 +0900

    Fix: 스프링의 xml Bean 설정
    예제 - 참조만

---
+++ src/test/java/hello/core/xml/XmlAppContextTest.java
@@ -0,0 +1,19 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContextTest {
+
+    // xml의 경우 컴파일 없이 적용 가능 (?) 하기 때문에 그냥 알아두는 정도로만
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
