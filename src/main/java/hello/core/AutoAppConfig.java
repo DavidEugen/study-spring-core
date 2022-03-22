cae1e35a1e99ab02f94ca606d74c999ea95fd080
DavidEugen <platonicojju@gmail.com>
2022-03-22 23:49:12 +0900

    Fix: @ComponentScan
    @Bean 이나 <bean> 으로 직접 등로도 가능했지만..
    등록해야 할 빈이 많을때 일괄로 가져오는 방법 @ComponentScan
    
    그에 따른 의존관계를 주입할때는 @Autowired. ( 직접 의존관계 명시해 주던걸 어노테이션으로 가능 )

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes =
+                Configuration.class))
+//@Configuration component상속, 기존에 테스트 하던 수동 등록 빈들도 다 불러오기에 제외 시킨다.
+public class AutoAppConfig {
+}
