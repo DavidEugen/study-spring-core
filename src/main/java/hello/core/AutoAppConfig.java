09dd764ffee3b7b56f7c25d4b282bc65f5ab10b4
DavidEugen <platonicojju@gmail.com>
2022-03-22 23:58:41 +0900

    Fix: @ComponentScan basePackage 설정
    의존관계를 주입할때는 @Autowired.
    이때 기본 전략은 type이 같은 빈을 주입한다.
    
    basePackages : 탐색할 패키지의 시작 위치를 지정
    basePackageClasses : 지정한 클래스의 패키지를 탐색 시작 위치로 지정한다
    지정하지 않을 시 @ComponentScand 이 붙은 설정정보 클래스의 패키지가 시작 위치가 된다.
    
    보통 최상위 클래스에 설정 클래스를 두고 @ComponentScan을 사용한다.
    
    스프링 부트의 경우 프로젝트의 루트 패키지에 @SpringBootApplication 두고 사용한다. (이 안에 @ComponentSacn 포함

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes =
                 Configuration.class))
 //@Configuration component상속, 기존에 테스트 하던 수동 등록 빈들도 다 불러오기에 제외 시킨다.
