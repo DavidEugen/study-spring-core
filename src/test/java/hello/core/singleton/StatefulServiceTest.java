f9e315695463e9ba9ebaaa716d750be86ac7568c
DavidEugen <platonicojju@gmail.com>
2022-03-22 16:50:01 +0900

    Feat: SingleTone 의 단점
    singleton 은 하나의 객체를 공유하기 때문에 문제점이 발생할 수 있다.
    => 상태를 유지하게(Stateful) 설계하면 안된다.

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,44 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    @DisplayName("싱글톤 컨테이너가 상태를 가지게 될때 문제점")
+    void statefulServiceSingleton(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+
+        //A 사용자 10000원 주문
+        statefulService1.order("userA",10000);
+        //B 사용자 20000원 주문
+        statefulService2.order("userB",20000);
+
+        //A 사용자의 금액 10000원 예상
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+
+    }
+
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+}
