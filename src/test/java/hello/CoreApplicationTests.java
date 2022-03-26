f18095cd0dc8ccbcdd42c958c4b6f85235552039
DavidEugen <platonicojju@gmail.com>
2022-03-26 21:48:38 +0900

    Fix: AutoAppConfig 는 왜 호출 되었는가?
    일단 현재 상황이 hello.core 에서 AutoAppConfig 와 AppConfig 가 @Configuration 을 달고 있다.
    
    그래서 @ComponentScan 으로 excludeFilters 로 Configuration 을 읽어 오지 못하도록 설정 하였으나
    여전히 똑같다.
    
    그렇다면 AppConfig, AutoAppConfig 은 최소한 @SpringBootTest 와 같은 레벨에서 호출되었을 가능성 있음
    (현재 모두 hello.core 아래에 있으므로.. )
    
    확인을 위해 @SpringBootTest 을 상위 패키지인 hello 아래로 이동.
    
    에러 로그 :
    Unable to find a @SpringBootConfiguration, you need to use @ContextConfiguration or @SpringBootTest(classes=...) with your test
    
    @SpringBootConfiguration 이 같은 레벨에 있어야 할듯 ..

--- src/test/java/hello/CoreApplicationTests.java
+++ src/test/java/hello/CoreApplicationTests.java
@@ -1,4 +1,4 @@
-package hello.core;
+package hello;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @SpringBootTest
+@ComponentScan(
+		excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 class CoreApplicationTests {
 
 	@Test
