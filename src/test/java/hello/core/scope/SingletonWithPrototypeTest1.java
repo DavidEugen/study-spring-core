63446c86a648f5c28a5895813f5d91fd95661fc0
DavidEugen <platonicojju@gmail.com>
2022-04-05 23:59:11 +0900

    Fix: 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점
    clientBean 이라는 싱글톤 빈이
    의존관계 주입을 통해서 프로토타입 빈을 주입받아서 사용하는 예
    
    1. 생성
    - clientBean 은 싱글톤 => 스프링 컨테이너 생성 시점에 함께 생성되고, 의존관계 주입도 발생.
    - clientBean 은 의존관계 자동 주입을 사용한다. 주입 시점에 스프링 컨테이너에 프로토타입 빈을 요청.
    - 스프링 컨테이너는 프로토타입 빈을 생성해서 clientBean 에 반환. 이때 프로토타입 빈의 count 필드 값은 0이다.
    - 이제 clientBean 은 프로토타입 빈을 내부 필드에 보관한다. ( 참조값을 보관한다.)
    
    2. clientBean1
    - 클라이언트1은 clientBean 을 스프링 컨테이너에 요청해서 받음. (싱글톤이므로 항상 같은 clientBean 이 반환.)
    - 클라이언트1은 clientBean.logic() 을 호출.
    - clientBean 은 prototypeBean의 addCount() 를 호출해서 프로토타입 빈의 count를 증가. count => 1
    
    3. 클라이언트 2
    - 클라이언트2는 clientBean 을 스프링 컨테이너에 요청해서 받음. (싱글톤이므로 항상 같은 clientBean 이 반환.)
    <<
       clientBean이 내부에 가지고 있는 프로토타입 빈은 이미 과거에 주입이 끝난 빈!!
       주입 시점에 스프링 컨테이너에 요청해서 프로토타입 빈이 새로 생성. ( 사용 할 때마다 새로 생성되는 것 아님! )
    >>
    - 클라이언트 2는 clientBean.logic() 을 호출한다.
    - clientBean 은 prototypeBean의 addCount() 를 호출해서 프로토타입 빈의 count를 증가. count => 2

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
@@ -12,20 +13,47 @@ import javax.annotation.PreDestroy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SingletonWithPrototypeTest1 {
+//    @Test
+//    void prototypeFind() {
+//        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+//
+//        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+//        prototypeBean1.addCount();
+//        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+//
+//        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+//        prototypeBean2.addCount();
+////        assertThat(prototypeBean2.getCount()).isEqualTo(2); // singleTon 일때
+//        assertThat(prototypeBean2.getCount()).isEqualTo(1); // protoType 일때
+//    }
+
     @Test
-    void prototypeFind() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
 
-        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
-        prototypeBean1.addCount();
-        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
 
-        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
-        prototypeBean2.addCount();
-//        assertThat(prototypeBean2.getCount()).isEqualTo(2); // singleTon 일때
-        assertThat(prototypeBean2.getCount()).isEqualTo(1); // protoType 일때
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(2);
     }
 
+    static class ClientBean {
+        private final PrototypeBean prototypeBean;
+
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
 
     // //일반 Singleton 일때는 기본으로 생성
     @Scope("prototype")
