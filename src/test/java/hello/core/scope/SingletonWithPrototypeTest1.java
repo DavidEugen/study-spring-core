2f5e84ce863125166ee4bcec4699f1366efd1db7
DavidEugen <platonicojju@gmail.com>
2022-04-05 23:43:21 +0900

    Fix: 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점
    스프링 컨테이너에 프토토타입 스코프의 빈을 요청하면 항상 새로운 객체 인스턴스를 생성해서 반환한다.
    
    코드를 보면 singleton 빈과는 달리

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,6 +3,7 @@ package hello.core.scope;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
@@ -10,34 +11,47 @@ import javax.annotation.PreDestroy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class PrototypeTest {
-
+public class SingletonWithPrototypeTest1 {
     @Test
-    public void prototypeBeanFind() {
+    void prototypeFind() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
-        System.out.println("find prototypeBean1");
 
         PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
-        System.out.println("find prototypeBean2");
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
 
         PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
-        System.out.println("prototypeBean1 = " + prototypeBean1);
-        System.out.println("prototypeBean2 = " + prototypeBean2);
-
-        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
-        ac.close();
+        prototypeBean2.addCount();
+//        assertThat(prototypeBean2.getCount()).isEqualTo(2); // singleTon 일때
+        assertThat(prototypeBean2.getCount()).isEqualTo(1); // protoType 일때
     }
 
+
+    // //일반 Singleton 일때는 기본으로 생성
     @Scope("prototype")
     static class PrototypeBean {
+
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
         @PostConstruct
         public void init() {
-            System.out.println("PrototypeBean.init");
+            System.out.println("PrototypeBean.init " + this); //singleton 일때 한번만 생성되는 것을 확인 할 수 있다.
+            //prototype 일때는 사용할때 마다 생성되는 것을 확인 할 수 있다.
         }
+
         @PreDestroy
         public void destroy() {
             System.out.println("PrototypeBean.destroy");
         }
+
     }
 
 }
